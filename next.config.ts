be2f287e1a7685e7e02f3a0b0f86cd1aade17ce5
Sai Kalyan <saikalyanblog@gmail.com>
2025-12-18 00:39:49 +0530

    feat: Configure 301 redirect from root to /today

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/today',
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
